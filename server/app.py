9fffcae0f6191bb413bc62e4ff93a1407c8af8be
moris96 <moriskhoudari@gmail.com>
2023-11-02 11:39:56 -0400

    login & register routes work

--- server/app.py
+++ server/app.py
@@ -13,6 +13,9 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+
+# ROUTES
+#REGISTER
 @app.route("/register", methods=["POST"])
 def register_user():
     email = request.json["email"]
@@ -22,6 +25,7 @@ def register_user():
 
     if user_exists:
         return jsonify({"error": "User already exists"}), 409
+        # abort(409)
 
     hashed_password = bcrypt.generate_password_hash(password)
     new_user = User(email=email, password=hashed_password)
@@ -34,6 +38,27 @@ def register_user():
         "id": new_user.id,
         "email": new_user.email
     })
+
+#LOGIN
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email=email).first() 
+
+    if user is None:
+        return jsonify({"error": "Unauthorized"}), 401
+        # abort(409)
+    
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    return jsonify({
+        "id": user.id,
+        "email": user.email 
+    })
+
 
 
 if __name__ == "__main__":
