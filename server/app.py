d412682b9455deadcdd0f6043d54bf00d699bb58
moris96 <moriskhoudari@gmail.com>
2023-11-03 11:35:44 -0400

    added react-frontend and fixing backend flask bugs for login

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, abort, session 
-from flask.json import jsonify
+from flask import Flask, request, jsonify, session
+# from flask.json import jsonify
+from flask_session import Session
 from flask_bcrypt import Bcrypt
 from config import ApplicationConfig
 from models import db, User
@@ -8,10 +9,24 @@ app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+@app.route("/@me")
+def get_current_user():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id": user.id,
+        "email": user.email 
+    })
 
 
 # ROUTES
@@ -45,18 +60,19 @@ def login_user():
     email = request.json["email"]
     password = request.json["password"]
 
-    user = User.query.filter_by(email=email).first() 
+    user = User.query.filter_by(email=email).first()
 
     if user is None:
         return jsonify({"error": "Unauthorized"}), 401
-        # abort(409)
-    
+
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Unauthorized"}), 401
     
+    session["user_id"] = user.id
+
     return jsonify({
         "id": user.id,
-        "email": user.email 
+        "email": user.email
     })
 
 
